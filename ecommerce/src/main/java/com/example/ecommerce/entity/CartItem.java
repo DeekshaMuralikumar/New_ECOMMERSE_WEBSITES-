e46b7d0017af0d7a925401d4c59032473ddce8f2
Deeksha Muralikumar <deekshamkt@gmail.com>
2026-03-14 21:32:26 +0530

    Entity Cart

---
+++ ecommerce/src/main/java/com/example/ecommerce/entity/CartItem.java
@@ -0,0 +1,38 @@
+package com.example.ecommerce.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cart_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    @ManyToOne
+    private Cart cart;
+
+    @ManyToOne
+    private Product product;
+} {
+    
+}
