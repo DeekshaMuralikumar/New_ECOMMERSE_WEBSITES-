21b8998f9b2f70c335e68a8b72790a207ac70d58
Deeksha Muralikumar <deekshamkt@gmail.com>
2026-03-15 19:24:08 +0530

    Repo Update

--- ecommerce/src/main/java/com/example/ecommerce/repository/CartRepository.java
+++ ecommerce/src/main/java/com/example/ecommerce/repository/CartRepository.java
@@ -1,14 +1,13 @@
 package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.Cart;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
-
     @Query("SELECT c FROM Cart c WHERE c.user.id = :userId")
     Optional<Cart> findCartByUser(@Param("userId") Long userId);
-
 }
--- ecommerce/src/main/java/com/example/ecommerce/repository/OrderRepository.java
+++ ecommerce/src/main/java/com/example/ecommerce/repository/OrderRepository.java
@@ -2,19 +2,22 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.entity.Order;
 import com.example.ecommerce.enums.OrderStatus;
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
     @Query("SELECT o FROM Order o JOIN FETCH o.orderItems WHERE o.id = :orderId")
     Order fetchOrderWithItems(@Param("orderId") Long orderId);
 
     @Query("SELECT o FROM Order o WHERE o.customer.id = :userId")
     List<Order> findOrdersByUser(@Param("userId") Long userId);
+
+    @Query("SELECT DISTINCT o FROM Order o JOIN o.orderItems oi JOIN oi.product p WHERE p.owner.id = :sellerId")
+    List<Order> findOrdersBySeller(@Param("sellerId") Long sellerId);
 
     @Query("SELECT o FROM Order o WHERE o.status = :status")
     List<Order> findOrdersByStatus(@Param("status") OrderStatus status);
@@ -23,8 +26,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     Double getTodayRevenue();
 
     @Query("SELECT o FROM Order o WHERE o.createdAt BETWEEN :start AND :end")
-    List<Order> findOrdersBetweenDates(
-            @Param("start") LocalDateTime start,
-            @Param("end") LocalDateTime end
-    );
+    List<Order> findOrdersBetweenDates(@Param("start") LocalDateTime start, @Param("end") LocalDateTime end);
+
+    @Query("SELECT o FROM Order o WHERE o.status = :status AND o.updatedAt < :time")
+    List<Order> findOrdersByStatusAndDeliveredBefore(@Param("status") OrderStatus status, @Param("time") LocalDateTime time);
 }
