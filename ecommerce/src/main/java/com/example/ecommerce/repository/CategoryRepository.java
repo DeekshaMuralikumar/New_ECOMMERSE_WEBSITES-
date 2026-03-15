1287d339aefdca483d6a4d3d9c1e13896b94d03a
Praveenkumar M <praveen.mspk494@gmail.com>
2026-03-15 19:54:55 +0530

    product, category and ad repo are updated

--- ecommerce/src/main/java/com/example/ecommerce/repository/CategoryRepository.java
+++ ecommerce/src/main/java/com/example/ecommerce/repository/CategoryRepository.java
@@ -3,6 +3,9 @@ package com.example.ecommerce.repository;
 import com.example.ecommerce.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+import java.util.Optional;
 
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByNameIgnoreCase(String name);
+    Optional<Category> findByNameIgnoreCase(String name);
 }
